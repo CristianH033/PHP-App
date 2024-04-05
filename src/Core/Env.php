cdf8ebde8aa4feb35bae4fc0413fa6f629efcf57
Cristian David Home <cristian_david033@hotmail.com>
2024-04-05 14:34:15 +0000

    Cast .env values.

--- src/Core/Env.php
+++ src/Core/Env.php
@@ -15,14 +15,35 @@ class Env
     public static function get(string $key, $default = null)
     {
         if (isset($_ENV[$key])) {
-            return match ($_ENV[$key]) {
-                'true' => true,
-                'false' => false,
-                default => $_ENV[$key],
-            };
+            return self::cast($_ENV[$key]);
         }
 
         return $default;
+    }
+
+    private static function cast(string $value): int|float|bool|string|null
+    {
+        if (is_numeric($value)) {
+            if (strpos($value, '.') === false) {
+                return (int) $value;
+            } else {
+                return (float) $value;
+            }
+        }
+
+        if ($value === 'true') {
+            return true;
+        }
+
+        if ($value === 'false') {
+            return false;
+        }
+
+        if (empty($value)) {
+            return null;
+        }
+
+        return $value;
     }
 
     public static function set(string $key, $value)
